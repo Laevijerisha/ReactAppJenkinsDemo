ddecc2dd05f912dceff6435418c157e61d9c4577
Laevijerisha <laevijerisha05@gmail.com>
2024-03-12 15:44:58 +0530

    np

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import Jest from './Jest';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    
+    <App/>
     <Jest/>
   </React.StrictMode>
 );
